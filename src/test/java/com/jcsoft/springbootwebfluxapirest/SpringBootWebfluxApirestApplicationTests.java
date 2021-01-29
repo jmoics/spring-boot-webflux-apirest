884e2877356d4eb9bf682a6911c560fa96c710f3
Jorge Cueva <jorge.cueva@globant.com>
2021-01-28 23:47:09 -0500

    initializing project.

---
+++ src/test/java/com/jcsoft/springbootwebfluxapirest/SpringBootWebfluxApirestApplicationTests.java
@@ -0,0 +1,15 @@
+package com.jcsoft.springbootwebfluxapirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebfluxApirestApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
